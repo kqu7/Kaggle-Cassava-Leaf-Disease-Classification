2af2471d9c0b6f366ff3f81877da6c8bcd7502a6
kqu7 <kqy7yu@gmail.com>
2021-02-03 16:10:45 -0500

    adding efficientnet training code & utility code

---
+++ util.js
@@ -0,0 +1,29 @@
+// prevent google colab from disconnecting
+function ClickConnect(){
+  console.log("Connnect Clicked - Start"); 
+  document.querySelector("#top-toolbar > colab-connect-button").shadowRoot.querySelector("#connect").click();
+  console.log("Connnect Clicked - End"); 
+};
+setInterval(ClickConnect, 60000)
+
+
+// with start/stop control
+var startClickConnect = function startClickConnect(){
+    var clickConnect = function clickConnect(){
+        console.log("Connnect Clicked - Start");
+        document.querySelector("#top-toolbar > colab-connect-button").shadowRoot.querySelector("#connect").click();
+        console.log("Connnect Clicked - End"); 
+    };
+
+    var intervalId = setInterval(clickConnect, 60000);
+
+    var stopClickConnectHandler = function stopClickConnect() {
+        console.log("Connnect Clicked Stopped - Start");
+        clearInterval(intervalId);
+        console.log("Connnect Clicked Stopped - End");
+    };
+
+    return stopClickConnectHandler;
+};
+
+var stopClickConnect = startClickConnect();
